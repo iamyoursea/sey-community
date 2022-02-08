40c01a32a7a431edfcbe1046e949bdf8baba185e
Sey <iamyoursea0@gmail.com>
2022-02-08 15:34:01 +0900

    Commit

--- src/main/java/com/sey/community/springboot/web/dto/HelloResponseDTO.java
+++ src/main/java/com/sey/community/springboot/web/dto/HelloResponseDTO.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public class BasicResponseDTO {
+public class HelloResponseDTO {
 
     private final String name;
     private final int amount;
