9ca4b1835d25ecdb632070d106609c2e6efeb2a7
Sey <iamyoursea0@gmail.com>
2022-02-09 12:14:03 +0900

    Commit

--- src/main/java/com/sey/community/springboot/web/IndexController.java
+++ src/main/java/com/sey/community/springboot/web/IndexController.java
@@ -224,12 +224,12 @@ public class IndexController {
         return "posts-view";
     }
 
-    @GetMapping("/game")
+    @GetMapping("/games")
     public String game(Model model, @LoginUser SessionUser loginUser) {
         if(loginUser!=null){
             model.addAttribute("loginUser", loginUser);
         }
-        return ("sey-games");
+        return ("/games/sey-games");
     }
 
     @GetMapping("/search")
