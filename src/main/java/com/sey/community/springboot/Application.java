3fa512c09e81dff34506770f9b629302c25e23ab
Sey <iamyoursea0@gmail.com>
2022-02-14 17:58:33 +0900

    파일업로드 구현

--- src/main/java/com/sey/community/springboot/Application.java
+++ src/main/java/com/sey/community/springboot/Application.java
@@ -2,18 +2,11 @@ package com.sey.community.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 //@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
-
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.yml";
-
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class)
-                .properties(APPLICATION_LOCATIONS)
-                .run(args);
+        SpringApplication.run(Application.class, args);
     }
 }
