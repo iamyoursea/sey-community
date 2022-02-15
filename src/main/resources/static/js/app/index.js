946b18d895eea6ffc6e42da5525560c6cb8a62e4
Sey <iamyoursea0@gmail.com>
2022-02-15 22:43:13 +0900

    파일업로드 구현

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -43,7 +43,7 @@ const main = {
             url: "/api/v1/" + writeTo,
             dataType: "json",
             contentType: "multipart/form-data;",
-            data: formData
+            data: JSON.stringify(formData)
         }).done(() => {
             alert("글이 등록되었습니다.")
             window.location.href = "/" + (writeTo !== "posts" ? writeTo : "")
